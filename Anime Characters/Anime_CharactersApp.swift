241a49c8d0270c350c15bed8a04e2d1b79089db1
Tom Bredemeier <tbredemeier@barrington220.org>
2024-03-12 11:22:58 -0500

    Initial Commit

---
+++ Anime Characters/Anime_CharactersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Anime_CharactersApp.swift
+//  Anime Characters
+//
+//  Created by Tom Bredemeier on 3/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Anime_CharactersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
